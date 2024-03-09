ddd58cf7327fe4e014f80d3830cd9bf1d3c30195
Mike Battaglia <70967369+mike-battaglia@users.noreply.github.com>
2024-03-09 11:40:47 -0600

    Update squeestats.rb

--- squeestats.rb
+++ squeestats.rb
@@ -1,4 +1,3 @@
-input SqueezeType = { Low, default Mid, High, All};
 input LongShortDefinition = { default HistAboveBelowZero, HistRisingFalling};
 
 def nBB = 2.0; 
@@ -8,45 +7,51 @@ def nK_Mid = 1.5;
 def nK_Low = 2.0;  
 def price = close;
 
-def selectedNK = if SqueezeType == SqueezeType.Mid then nK_Mid else if SqueezeType == SqueezeType.Low then nK_Low else if SqueezeType == SqueezeType.High then nK_High else nK_Mid;
-
-def inSqueeze = TTM_Squeeze(price =  price, length = Length, nk = selectedNK, nBB = nBB ).SqueezeAlert == 0;
-def squeezeMomentum = TTM_Squeeze(price =  price, length = Length, nk = selectedNK, nBB = nBB );
-
-def fired = !inSqueeze and inSqueeze[1];
-def firedLong = fired and ((LongShortDefinition == LongShortDefinition.HistAboveBelowZero and  squeezeMomentum > 0) or (LongShortDefinition == LongShortDefinition.HistRisingFalling and  squeezeMomentum > squeezeMomentum[1]));
-
-def squeezeCount = TotalSum(fired);
-
-def inSqueezeLow = TTM_Squeeze(price =  price, length = Length, nk = nK_Low, nBB = nBB ).SqueezeAlert == 0;
-def squeezeMomentumLow = TTM_Squeeze(price =  price, length = Length, nk = nK_Low, nBB = nBB );
-
-def firedLow = !inSqueezeLow and inSqueezeLow[1];
-
-def firedLongLow = firedLow and ((LongShortDefinition == LongShortDefinition.HistAboveBelowZero and  squeezeMomentumLow > 0) or (LongShortDefinition == LongShortDefinition.HistRisingFalling and  squeezeMomentumLow > squeezeMomentumLow[1]));
-
-def squeezeCountLow = TotalSum(firedLow);
-
-
-def inSqueezeHigh = TTM_Squeeze(price =  price, length = Length, nk = nK_High, nBB = nBB ).SqueezeAlert == 0;
-def squeezeMomentumHigh = TTM_Squeeze(price =  price, length = Length, nk = nK_High, nBB = nBB );
-
-def firedHigh = !inSqueezeHigh and inSqueezeHigh[1];
-def firedLongHigh = firedHigh and ((LongShortDefinition == LongShortDefinition.HistAboveBelowZero and  squeezeMomentumHigh > 0) or (LongShortDefinition == LongShortDefinition.HistRisingFalling and  squeezeMomentumHigh > squeezeMomentumHigh[1]));
-
-def squeezeCountHigh = TotalSum(firedHigh);
-
-
 def BolKelDelta_Mid = reference BollingerBands("num_dev_up" = 2.0, "length" = 20.0 )."upperband" - KeltnerChannels("factor" = 1.5, "length" = 20.0)."Upper_Band";
 def BolKelDelta_Low = reference BollingerBands("num_dev_up" = 2.0, "length" = 20.0 )."upperband" - KeltnerChannels("factor" = 2.0, "length" = 20.0)."Upper_Band";
 def BolKelDelta_High = reference BollingerBands("num_dev_up" = 2.0, "length" = 20.0 )."upperband" - KeltnerChannels("factor" = 1.0, "length" = 20.0)."Upper_Band";
 
-def tSumFiredLow = TotalSum(firedLow);
-def tSumFiredMid = TotalSum(fired);
-def tSumFiredHigh = TotalSum(firedHigh);
+def sqLow = BolKelDelta_Low <= 0;
+def sqMid = BolKelDelta_Mid <= 0;
+def sqHigh = BolKelDelta_High <= 0;
 
-AddLabel( BolKelDelta_Low <= 0,"Low "+tSumFiredLow+" ("+(firedLongLow/tSumFiredLow)+")");
-AddLabel( BolKelDelta_Mid <= 0,"Mid "+tSumFiredMid+" ("+(firedLong/tSumFiredMid)+")");
-#these 2 below break it
-AddLabel( BolKelDelta_High <= 0,"High "+tSumFiredHigh+" ("+(firedLongHigh/tSumFiredHigh)+")");
-AddLabel( !(BolKelDelta_Low <= 0) and !(BolKelDelta_Mid <= 0) and !(BolKelDelta_High <= 0), "None");
+def selectedNK = if sqMid then nK_Mid else if sqLow then nK_Low else if sqHigh then nK_High else nK_Mid;
+
+def inSqueeze = TTM_Squeeze(price =  price, length = Length, nk = selectedNK, nBB = nBB ).SqueezeAlert == 0;
+def squeezeMomentum = TTM_Squeeze(price =  price, length = Length, nk = selectedNK, nBB = nBB );
+def upMomentumStart = squeezeMomentum > squeezeMomentum[1] and squeezeMomentum[1] <= squeezeMomentum[2];
+def downMomentumStart = squeezeMomentum < squeezeMomentum[1] and squeezeMomentum[1] >= squeezeMomentum[2];
+def upMomentumLength = if upMomentumStart then 1 else if !downMomentumStart then upMomentumLength[1] + 1 else 0;
+def completedUpMomentumLength = if downMomentumStart then upMomentumLength[1] else 0;
+def downMomentumLength = if downMomentumStart then 1 else if !upMomentumStart then downMomentumLength[1] + 1 else 0;
+def completedDownMomentumLength = if upMomentumStart then downMomentumLength[1] else 0;
+def upCycles = TotalSum(upMomentumStart);
+def downCycles = TotalSum(downMomentumStart);
+def aveUpCycleLength = Round(TotalSum(completedUpMomentumLength) / upCycles, 0);
+def aveDownCycleLength = Round(TotalSum(completedDownMomentumLength) / downCycles, 0);
+
+def fired = !inSqueeze and inSqueeze[1];
+def firedLong = fired and ((LongShortDefinition == LongShortDefinition.HistAboveBelowZero and  squeezeMomentum > 0) or (LongShortDefinition == LongShortDefinition.HistRisingFalling and  squeezeMomentum > squeezeMomentum[1]));
+def firedShort = fired and ((LongShortDefinition == LongShortDefinition.HistAboveBelowZero and  squeezeMomentum <= 0) or (LongShortDefinition == LongShortDefinition.HistRisingFalling and  squeezeMomentum < squeezeMomentum[1]));
+def postSqueezeRun = if fired then 1 else if inSqueeze or downMomentumStart or upMomentumStart then 0 else postSqueezeRun[1];
+def postSqueezeRunLength = if postSqueezeRun and !(downMomentumStart or upMomentumStart) then postSqueezeRunLength[1] + 1 else 0;
+def completedPostSqueezeRun = if !postSqueezeRun and postSqueezeRun[1] then postSqueezeRunLength[1] else 0;
+def postSqueezeRuns =  if !postSqueezeRun and postSqueezeRun[1] then 1 else 0;
+def avePostSqueezeRun = TotalSum(completedPostSqueezeRun) / TotalSum(fired);
+def priceFired = if fired then close[1] else priceFired[1];
+def completedPriceMovePercent =  if !postSqueezeRun and postSqueezeRun[1] then AbsValue(100 * (close[1] - priceFired[1]) / priceFired[1]) else 0;
+def avePostSqueezePercent = TotalSum(completedPriceMovePercent) / TotalSum(fired);
+
+def squeezeLength = if inSqueeze then squeezeLength[1] + 1 else 0;
+def completedSqueezeLength = if fired then squeezeLength[1] else 0;
+def squeezeCount = TotalSum(fired);
+def aveSqueezeLength = Round(TotalSum(completedSqueezeLength) / squeezeCount, 0);
+
+def theRatio = firedLong/squeezeCount;
+
+AddLabel( sqLow,"Low, Count="+squeezeCount+", Long="+firedLong+", Short="+firedShort);
+AddLabel( sqMid,"Mid, Count="+squeezeCount+", Long="+firedLong+", Short="+firedShort);
+AddLabel( sqHigh,"High, Count="+squeezeCount+", Long="+firedLong+", Short="+firedShort);
+AddLabel( !(sqLow) and !(sqMid) and !(sqHigh), "None");
+
+AssignBackgroundColor( if sqLow and !sqMid and !sqHigh then color.dark_green else if sqMid and !sqHigh then color.dark_red else if sqHigh then color.dark_orange else color.black);
