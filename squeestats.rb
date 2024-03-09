c72f8c275b49d93b9eb7e2dd0023779072453dfb
Mike Battaglia <70967369+mike-battaglia@users.noreply.github.com>
2024-03-09 09:37:46 -0600

    Update squeestats.rb

--- squeestats.rb
+++ squeestats.rb
@@ -1,25 +1,18 @@
 def inSqueezeLow = TTM_Squeeze(price = close, length = 20.0, nk = 2.0, nBB = 2.0 );
-def squeezeMomentumLow = TTM_Squeeze(price = close, length = 20.0, nk = 2.0, nBB = 2.0 );
 def firedLow = !inSqueezeLow and inSqueezeLow[1];
-def firedLongLow = firedLow and (squeezeMomentumLow > 0);
+def firedLongLow = firedLow and (inSqueezeLow > 0);
 
 def inSqueezeMid = TTM_Squeeze(price = close, length = 20.0, nk = 1.5, nBB = 2.0 );
-def squeezeMomentumMid = TTM_Squeeze(price = close, length = 20.0, nk = 1.5, nBB = 2.0 );
 def firedMid = !inSqueezeMid and inSqueezeMid[1];
-def firedLongMid = firedMid and (squeezeMomentumMid > 0);
+def firedLongMid = firedMid and (inSqueezeMid > 0);
 
 def inSqueezeHigh = TTM_Squeeze(price = close, length = 20.0, nk = 1.0, nBB = 2.0 );
-def squeezeMomentumHigh = TTM_Squeeze(price = close, length = 20.0, nk = 1.0, nBB = 2.0 );
 def firedHigh = !inSqueezeHigh and inSqueezeHigh[1];
-def firedLongHigh = firedHigh and (squeezeMomentumHigh > 0);
+def firedLongHigh = firedHigh and (inSqueezeHigh > 0);
 
 def BolKelDelta_Mid = reference BollingerBands("num_dev_up" = 2.0, "length" = 20.0 )."upperband" - KeltnerChannels("factor" = 1.5, "length" = 20.0)."Upper_Band";
 def BolKelDelta_Low = reference BollingerBands("num_dev_up" = 2.0, "length" = 20.0 )."upperband" - KeltnerChannels("factor" = 2.0, "length" = 20.0)."Upper_Band";
 def BolKelDelta_High = reference BollingerBands("num_dev_up" = 2.0, "length" = 20.0 )."upperband" - KeltnerChannels("factor" = 1.0, "length" = 20.0)."Upper_Band";
-
-#def sLow = BolKelDelta_Low <= 0;
-#def sMid = BolKelDelta_Mid <= 0;
-#def sHigh = BolKelDelta_High <= 0;
 
 def tSumFiredLow = TotalSum(firedLow);
 def tSumFiredMid = TotalSum(firedMid);
